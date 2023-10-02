67f548c4c4f6b7dfdca2e86ef874b7469b511d2d
Katsiaryna Smuraha <Smuraha_K@iba>
2023-10-02 10:20:10 +0300

    add meals data fetch

--- meels/src/components/Meals/AvailableMeals/AvailableMeals.js
+++ meels/src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { DUMMY_MEALS } from "./dummy-meals";
+import React, {useState} from "react";
 import styles from "./AvailableMeals.module.css";
 import Card from "../../UI/Card/Card";
 import MealItem from "../MealItem/MealItem";
+import { useEffect } from "react";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      setLoading(true);
+      const responce = await fetch('https://react-learn-project-c7c1a-default-rtdb.firebaseio.com/meals.json');
+      if (responce.ok) {
+        const data = await responce.json();
+      console.log(data);
+      const fetchedMeals = [];
+      for(let key in data) {
+        fetchedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price
+        })
+      }
+      setMeals(fetchedMeals);
+      setLoading(false);
+      } else {
+        setLoading(false);
+        throw new Error(responce.status);
+      }
+      
+    }
+
+    try{
+      fetchMeals();
+    } catch(error) {
+      setError(error.message);
+    }
+  }, [])
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
@@ -18,7 +55,9 @@ const AvailableMeals = () => {
   return (
     <section className={styles.meals}>
       <Card>
-        <ul>{mealsList}</ul>
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {meals.length > 0 && <ul>{mealsList}</ul>}
       </Card>
     </section>
   );
