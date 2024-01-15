4380b5ace49d8dae9b55ef70af53aa5fae8f76b0
Katsiaryna Smuraha <Smuraha_K@iba>
2024-01-15 09:29:20 +0300

    add minor chages

--- tailwind/src/components/CustomInput.jsx
+++ tailwind/src/components/CustomInput.jsx
@@ -1,6 +1,6 @@
 export default function CustomInput({ label, invalid, ...props }) {
-    let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase ";
-    let inputClasses = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow" 
+    let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase";
+    let inputClasses = "w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow"; 
     if (invalid) {
         labelClasses += ' text-red-400';
         inputClasses += ' text-red-500 bg-red-100 border-red-300';
